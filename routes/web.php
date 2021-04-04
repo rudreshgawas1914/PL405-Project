2c2d1e45faaf6590f796fa0aa55c0145e3c43828
1932Prajacta <mca.1932@unigoa.ac.in>
2021-04-05 01:41:53 +0530

    Complete New Dashboard

---
+++ routes/web.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\DashboardController;
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+//for both
+Route::group(['middleware'=>['auth']],function(){
+    Route::get('/dashboard',[DashboardController::class,'index'])->name('dashboard');
+});
+
+//for admin
+Route::group(['middleware'=>['auth', 'role:admin']],function(){
+    Route::get('/dashboard/traindetails',[DashboardController::class,'index'])->name('dashboard');
+});
+
+
+
+
+require __DIR__.'/auth.php';
