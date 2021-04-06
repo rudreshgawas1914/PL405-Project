237c10c67fc2a84c6b7a57d20bf079a9f5257cf5
sidhantk283 <sidhantk283@gmail.com>
2021-04-06 23:22:52 +0530

    ..

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -2,15 +2,15 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\train_route;
 use Illuminate\Http\Request;
-
 use Illuminate\Support\Facades\Auth;
 
 class DashboardController extends Controller
 {
     public function index(){
         if(Auth::user()->hasRole('admin')){
-                return view('/admin/admin_dashboard');
+                return view('/admin/admin_dashboard')->with('trainArr',train_route::all());
 
         }elseif(Auth::user()->hasRole('user')) {
                  return view('/user/user_dashboard');
