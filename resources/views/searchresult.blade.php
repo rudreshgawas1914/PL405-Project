32f32d3e2e1c5b0a6a1cda5312a67dd800aa1585
1932Prajacta <mca.1932@unigoa.ac.in>
2021-04-12 03:34:03 -0700

    search display

--- resources/views/searchresult.blade.php
+++ resources/views/searchresult.blade.php
@@ -3,27 +3,29 @@
 @section('content')
 <div class="search-panel1">
     <div class="row text-center">
-        <h4><strong>Search Results</strong></h4>
+        <h4><strong>Search Train</strong></h4>
     </div>
-    <form autocomplete="off" style="padding: 1% 1% 5% 1%" action="searchresult">
+    <form autocomplete="off" style="padding: 1% 1% 3% 1%" action="searchresult">
                 @csrf
                     <div class="row bg-warning">
-                        <div class="col-lg-2">
+                        <div class="col-lg-2" style="margin-right: 15px;">
                             <div class="row">
-                                <input type="text" class="form-control text-center search-panel1-input" id="From" name="from" placeholder="&#xf124 From" requiired>
+                                <input type="text" class="form-control text-center search-panel1-input" id="From" name="from" placeholder="&#xf124 From" required>
                             </div>
                         </div>
-                        <div class="col-lg-2">
+                        <div class="col-lg-2" style="margin-right: 15px;">
                             <div class="row">
                                 <input type="text" class="form-control text-center search-panel1-input" id="To" name="to" placeholder="&#xf3c5 To" required>
                             </div>
                         </div>
-                        <div class="col-lg-2">
+
+                        <!--
+                        <div class="col-lg-2" style="margin-right: 15px;">
                             <div class="row">
                                 <input type="date" class="form-control text-center search-panel1-input" id="on" name="date" placeholder="&#65 Date">
                             </div>
                         </div>
-                        <div class="col-lg-2">
+                        <div class="col-lg-2" style="margin-right: 15px;">
                             <div class="row">
                                 <select class="form-select text-center search-panel1-input" aria-label="Default select example">
                                     <option selected><i class="fas fa-briefcase"></i> All Classes</option>
@@ -33,7 +35,8 @@
                                 </select>
                             </div>
                         </div>
-                        <div class="col-lg-2">
+                    -->
+                        <div class="col-lg-2" >
                             <div class="row">
                                 <button type="submit" class="btn btn-primary search-panel1-input"><i class="fas fa-search"></i> Search</button>
                             </div>
@@ -45,13 +48,20 @@
 @if (strpos(strtoupper($train->Source_Location),strtoupper($from)) !== false)
     @if (strpos(strtoupper($train->Destination),strtoupper($to)) !== false)
         <div class="panel-create-train">
-            Train Name : {{$train->Train_Name}}({{$train->Train_No}})
-            {{$train->Status}}
-            {{$train->Source_Location}}
-            {{$train->Departure_Time}}
-            {{$train->Destination}}
-            {{$train->Arrival_Time}}
-            <button type="button" onclick="">Book Ticket</button>
+            
+            <div class="trainName">
+                <h4>{{$train->Train_Name}} &emsp; Train : {{$train->Train_No}} &nbsp;
+                <button type="button" class="btn btn-link">CHECK SEATS</button></h4></div>
+            
+            <div class="trainTime">
+                 {{$train->Departure_Time}} || {{$train->Source_Location}}
+                    &emsp;
+                 {{$train->Arrival_Time}} || {{$train->Destination}}
+            </div>
+            
+                
+            <div class="btn" style="margin-left: 70%;"><button type="button" class="btn btn-primary" onclick=""><i class="fas fa-search"></i>Book Ticket</button></div>
+            
         </div>
     @endif
 @endif
